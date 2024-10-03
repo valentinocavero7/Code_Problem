fb3ef911f08c4a1007a65342a11271d5d7199ce1
Mario_7 <111661567+valentinocavero7@users.noreply.github.com>
2024-10-03 16:58:37 -0500

    desarrollo problema B_Different_string

---
+++ CodeForces/B_Different String.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+        set<char> s;
+        for (int i = 0; i < str.size(); i++)
+        {
+            char ch = str[i];
+            s.insert(ch);
+        }
+        if(s.size() == 1) {
+            cout << "NO" << endl;
+        } else {
+            char ch = str[0];
+            str = str.substr(1);
+            str += ch;
+            cout << "YES" << endl;
+            cout << str << endl;
+        }
+        
+    }
+    
+}
+
+
